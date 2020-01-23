05a8cc97e69240a39f7620878fa30135502bcf6e
Vivek Tiwari <vivek.tiwari07@infosys.com>
2020-01-23 15:16:17 +0530

    Fixes for Task 2

--- libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -3,37 +3,28 @@ import {
   ChangeDetectorRef,
   Component,
   Input,
-  OnInit
+  OnInit,
+  OnDestroy
 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Chart } from '../models/chart.interface';
 
 @Component({
   selector: 'coding-challenge-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
-  @Input() data$: Observable<any>;
-  chartData: any;
+export class ChartComponent implements OnInit, OnDestroy {
+  @Input() public data$: Observable<any>;
+  @Input() public chart: Chart;
+  public chartData: any;
+  private subscription: Subscription;
+  constructor(private cd: ChangeDetectorRef) { }
 
-  chart: {
-    title: string;
-    type: string;
-    data: any;
-    columnNames: string[];
-    options: any;
-  };
-  constructor(private cd: ChangeDetectorRef) {}
-
-  ngOnInit() {
-    this.chart = {
-      title: '',
-      type: 'LineChart',
-      data: [],
-      columnNames: ['period', 'close'],
-      options: { title: `Stock price`, width: '600', height: '400' }
-    };
-
-    this.data$.subscribe(newData => (this.chartData = newData));
+  public ngOnInit() {
+    this.subscription = this.data$.subscribe(newData => (this.chartData = newData));
+  }
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
